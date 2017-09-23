a444634341b17cab25ebe139510187c714c8eb4f
pureliumy <alephantand@gmail.com>
2017-09-23 16:16:44 +0800

    storage banners into json file for better performance and faster response

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const superagent = require('superagent')
 const cheerio = require('cheerio')
 const Express = require('express')
+const fs = require('fs')
 
 const app = new Express()
+const jsonPath = './banners.json'
 
-app.get('/', function (req, res) {
+saveJson()
+let timer = setInterval(saveJson, 1000 * 60 * 10)
+
+function saveJson () {
   superagent.get('http://music.163.com/discover')
-    .end((err, data) => {
-      if (err) return res.send(err)
-      const $ = cheerio.load(data.text)
+    .end((err, res) => {
+      if (err) {
+        console.log(err)
+        this.saveJson()
+      }
+      const $ = cheerio.load(res.text)
       const item = $('#g_backtop+script').html()
       const index = item.indexOf('[')
       const lastIndex = item.lastIndexOf(';')
       const tempString = item.substring(index, lastIndex)
-      const jsonString = '{code:' + data.status + ',result:' + tempString + '}'
+      const jsonString = '{code:' + res.status + ',result:' + tempString + '}'
       const jsonData = eval('(' + jsonString + ')')
-      res.jsonp(jsonData)
+
+      fs.writeFile(jsonPath, JSON.stringify(jsonData, null, 2), (err) => {
+        if (err) console.log(err)
+        console.log('json have been saved to', jsonPath)
+      })
     })
+}
+
+app.get('/', (req, res) => {
+  const jsonData = fs.readFileSync(jsonPath)
+  res.jsonp(JSON.parse(jsonData))
 })
 
 app.listen(3000)
